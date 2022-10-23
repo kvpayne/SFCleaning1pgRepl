4314918c5434692be954d3fce66cd665cbb87d43
Kenneth V Payne <91850990+kvpayne@users.noreply.github.com>
2022-10-23 00:25:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+   alert("I am an alert box!");
+ }
+
+function myResidential() {
+  alert("Currently we are only taking referral clients for our residential services, please be prepared to provide us with the name of your referral by sending a text to: (480 819-2274)");
+}
